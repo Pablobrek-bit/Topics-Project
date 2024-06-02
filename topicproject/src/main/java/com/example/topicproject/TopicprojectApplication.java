76919f1a171ef0ae09e48a601110957f9d642732
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2024-06-02 20:05:39 -0300

    feat: initial configuration

---
+++ topicproject/src/main/java/com/example/topicproject/TopicprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.topicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicprojectApplication.class, args);
+	}
+
+}
---
+++ topicproject/src/test/java/com/example/topicproject/TopicprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.topicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
